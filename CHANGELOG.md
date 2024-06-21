7fedd798bd6da7a46143b767c0d11b851c71f5b5
Anderson Menezes <andersonsmenezes@gmail.com>
2024-06-21 14:46:51 -0300

    feat: Add support for contacts and purchaseInfo entities (#2)
    * feat: Add support for contacts and purchaseInfo entities
    
    * Lint fix
    
    * Increment changelog
    
    * Fix changelog
    
    ---------
    
    Co-authored-by: Anderson Menezes <anderson.menezes@vtex.com.br>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- Support for `contacts` and `purchaseInfo` entities.
+
 ## [0.0.1] - 2024-06-18
 
 ### Added
--- node/clients/psv2.ts
+++ node/clients/psv2.ts
@@ -1,6 +1,13 @@
 import type { InstanceOptions, IOContext } from '@vtex/api'
 import { ExternalClient } from '@vtex/api'
 
+import {
+  ADDRESS_ENTITY,
+  PROFILE_ENTITY,
+  PURCHASE_INFO_ENTITY,
+  CONTACTS_ENTITY,
+} from '../constants'
+
 export default class PSV2 extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
     super(`http://${context.account}.vtexcommercestable.com.br`, context, {
@@ -13,7 +20,31 @@ export default class PSV2 extends ExternalClient {
     })
   }
 
-  public async getProfile(
+  public async getDocument(
+    entity: string,
+    profileId: string,
+    documentId: string,
+    versionId: string
+  ): Promise<Record<string, unknown>> {
+    switch (entity) {
+      case PROFILE_ENTITY:
+        return this.getProfile(profileId, versionId)
+
+      case ADDRESS_ENTITY:
+        return this.getAddress(profileId, documentId, versionId)
+
+      case PURCHASE_INFO_ENTITY:
+        return this.getPurchaseInfo(profileId)
+
+      case CONTACTS_ENTITY:
+        return this.getContact(profileId, documentId, versionId)
+
+      default:
+        throw new Error(`The entity ${entity} is not supported by this app.`)
+    }
+  }
+
+  private async getProfile(
     profileId: string,
     versionId: string
   ): Promise<Record<string, unknown>> {
@@ -22,13 +53,31 @@ export default class PSV2 extends ExternalClient {
     )
   }
 
-  public async getAddress(
+  private async getAddress(
     profileId: string,
     addressId: string,
     versionId: string
   ): Promise<Record<string, unknown>> {
     return this.http.get(
       `/api/storage/profile-system/profiles/${profileId}/addresses/${addressId}/versions/${versionId}/unmask?onBehalfOf=psv2-webhook`
+    )
+  }
+
+  private async getPurchaseInfo(
+    profileId: string
+  ): Promise<Record<string, unknown>> {
+    return this.http.get(
+      `/api/storage/profile-system/profiles/${profileId}/purchase-info/unmask?onBehalfOf=psv2-webhook`
+    )
+  }
+
+  private async getContact(
+    profileId: string,
+    contactId: string,
+    versionId: string
+  ): Promise<Record<string, unknown>> {
+    return this.http.get(
+      `/api/storage/profile-system/profiles/${profileId}/contacts/${contactId}/versions/${versionId}/unmask?onBehalfOf=psv2-webhook`
     )
   }
 }
